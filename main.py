9555325210143ac45b10f1105b9466229f9189a2
vincecloutier <vincentcloutier@mail.com>
2022-08-03 12:53:12 -0400

    simplified cell class

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from queue import PriorityQueue
 
 pygame.display.set_caption("A* Visualizer")
 
-#Defining RGB Color Codes for our Cells
 BLACK = (0 , 0, 0) #Wall
 WHITE = (255, 255, 255) #Empty
 GRAY = (128, 128, 128) #Grid Lines
@@ -20,76 +19,50 @@ class Cell:
         self.x = row * size
         self.y = col * size
         self.color = WHITE
-        self.neighbors = []
+        self.neighbours = []
         self.size = size
         self.totalRows = totalRows
 
-    #Gets the position of the cell
-    def getPos(self):
-        return self.row, self.col
-    
-    #Methods that check the type of cell
-    def isClosed(self):
-        return self.color == RED
-    
-    def isOpen(self):
-        return self.color == GREEN
-    
-    def isWall(self):
-        return self.color == BLACK
-    
-    def isStart(self):
-        return self.color == ORANGE
-
-    def isEnd(self):
-        return self.color == PURPLE
-
-    #Methods that set/make cells 
     def makeClosed(self):
         self.color = RED
-    
     def makeOpen(self):
         self.color = GREEN
-    
     def makeWall(self):
         self.color = BLACK
-    
     def makeStart(self):
         self.color = ORANGE
-
     def makeEnd(self):
         self.color = PURPLE
-    
     def makePath(self):
         self.color = TURQUOISE
 
-    #Resets cell back to empty/white
+    def isClosed(self):
+        return self.color == RED
+    def isOpen(self):
+        return self.color == GREEN
+    def isWall(self):
+        return self.color == BLACK
+    def isStart(self):
+        return self.color == ORANGE
+    def isEnd(self):
+        return self.color == PURPLE
+
     def reset(self):
         self.color = WHITE
-
-    #Draws Cell
     def draw(self, window):
         pygame.draw.rect(window, self.color, (self.x, self.y, self.size, self.size))
-
-    #Checks and updates neighboring Cells
-    def update_neighbors(self, grid):
-        self.neighbors = []
-
-        #DOWN
-        if self.row < self.totalRows - 1 and not grid[self.row + 1][self.col].isWall():
-            self.neighbors.append(grid[self.row + 1][self.col])
-
-        #UP
-        if self.row > 0 and not grid[self.row - 1][self.col].isWall():
-            self.neighbors.append(grid[self.row - 1][self.col])
-        
-        #RIGHT
-        if self.col < self.totalRows - 1 and not grid[self.row][self.col + 1].isWall():
-            self.neighbors.append(grid[self.row][self.col + 1])
-
-        #LEFT
-        if self.row > 0 and not grid[self.row][self.col - 1].isWall():
-            self.neighbors.append(grid[self.row][self.col - 1])
+    def getPos(self):
+        return self.row, self.col
+    def updateNeighbours(self, grid):
+        self.neighbours = []
+        if self.row < self.totalRows - 1 and not grid[self.row + 1][self.col].isWall(): #DOWN
+            self.neighbours.append(grid[self.row + 1][self.col])
+        if self.row > 0 and not grid[self.row - 1][self.col].isWall():  #UP
+            self.neighbours.append(grid[self.row - 1][self.col])
+        if self.col < self.totalRows - 1 and not grid[self.row][self.col + 1].isWall(): #RIGHT
+            self.neighbours.append(grid[self.row][self.col + 1])
+        if self.row > 0 and not grid[self.row][self.col - 1].isWall(): #LEFT
+            self.neighbours.append(grid[self.row][self.col - 1])
 
     
 
@@ -100,9 +73,9 @@ def h(p1, p2):
     return abs(x1 - x2) + abs(y1 - y2)
 
 #Reconstructs shortest path between start and end to draw
-def reconstructPath(came_from, current, draw):
-    while current in came_from:
-        current = came_from[current]
+def reconstructPath(cameFrom, current, draw):
+    while current in cameFrom:
+        current = cameFrom[current]
         current.makePath()
         draw()
 
@@ -111,31 +84,33 @@ def aStarAlgo(draw, grid, start, end):
     count = 0
     openSet = PriorityQueue()
     openSet.put((0, count, start))
-    came_from = {}
+    openSetHash = {start}
+    cameFrom = {}
+
+
     g = {cell: float("inf") for row in grid for cell in row}
     g[start] = 0
     f = {cell: float("inf") for row in grid for cell in row}
     f[start] = h(start.getPos(), end.getPos())
-    openSetHash = {start}
 
     while not openSet.empty():
         current = openSet.get()[2]
         openSetHash.remove(current)
         if current == end:
-            reconstructPath(came_from, end, draw)
+            reconstructPath(cameFrom, end, draw)
             end.makeEnd()
             return True
-        for neighbor in current.neighbors:
+        for neighbour in current.neighbours:
             tempG = g[current] + 1
-            if tempG < g[neighbor]:
-                came_from[neighbor] = current
-                g[neighbor] = tempG
-                f[neighbor] = tempG + h(neighbor.getPos(), end.getPos())
-                if neighbor not in openSetHash:
+            if tempG < g[neighbour]:
+                cameFrom[neighbour] = current
+                g[neighbour] = tempG
+                f[neighbour] = tempG + h(neighbour.getPos(), end.getPos())
+                if neighbour not in openSetHash:
                     count += 1
-                    openSet.put((f[neighbor], count, neighbor))
-                    openSetHash.add(neighbor)
-                    neighbor.makeOpen()
+                    openSet.put((f[neighbour], count, neighbour))
+                    openSetHash.add(neighbour)
+                    neighbour.makeOpen()
         draw()
         if current != start:
             current.makeClosed()
@@ -226,7 +201,7 @@ def main(window, size):
                 if event.key == pygame.K_SPACE and startCell and endCell:
                     for row in grid:
                         for cell in row:
-                            cell.update_neighbors(grid)
+                            cell.updateNeighbours(grid)
                     aStarAlgo(lambda: draw(window, grid, rows, size), grid, startCell, endCell)
 
     pygame.quit()
