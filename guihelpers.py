038006fedfb15bed32c200059e258e87748dc1ab
vincecloutier <vincentcloutier@mail.com>
2023-07-08 14:20:09 -0400

    switched to buttons successfully, a lot of cleanliness still needed

--- guihelpers.py
+++ guihelpers.py
@@ -3,7 +3,49 @@ from cell import Cell
 from algorithms import pathfinder, astar_priority, dijkstra_priority
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH, GRID_COLUMNS, GRID_ROWS, GAP, COLOURS
 
-def makeGrid():
+
+class Button:
+    def __init__(self, color, x, y, width, height, text=''):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+
+    def draw(self, window, outline=None):
+        if outline:
+            pygame.draw.rect(window, outline, (self.x-2, self.y-2, self.width+4, self.height+4), 0)
+        pygame.draw.rect(window, self.color, (self.x, self.y, self.width, self.height), 0)
+        if self.text != '':
+            font = pygame.font.SysFont('comicsans', 50)
+            text = font.render(self.text, True, (0, 0, 0))
+            window.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
+
+    def isOver(self, pos):
+        if self.x < pos[0] < self.x + self.width:
+            if self.y < pos[1] < self.y + self.height:
+                return True
+        return False
+
+BUTTON_WIDTH, BUTTON_HEIGHT = 200, 50
+BUTTONS = [
+    Button(COLOURS['RED'], 0, SCREEN_HEIGHT - BUTTON_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT, 'A*'),
+    Button(COLOURS['GREEN'], BUTTON_WIDTH, SCREEN_HEIGHT - BUTTON_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT, 'Dijkstra'),
+    Button(COLOURS['BLACK'], BUTTON_WIDTH * 2, SCREEN_HEIGHT - BUTTON_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT, 'Reset'),
+]
+
+
+def pathfinding(window, grid, startCell, endCell, algorithm):
+    for row in grid:
+        for cell in row:
+            cell.updateNeighbours(grid)
+    if algorithm == 'astar':
+        pathfinder(lambda: draw(window, grid), grid, startCell, endCell, astar_priority)
+    elif algorithm == 'dijkstra':
+        pathfinder(lambda: draw(window, grid), grid, startCell, endCell, dijkstra_priority)
+
+def make_grid():
     grid = []
     for i in range(GRID_COLUMNS):
         grid.append([])
@@ -19,24 +61,27 @@ def draw(window, grid):
             cell.draw(window)
     for i in range(GRID_COLUMNS):
         pygame.draw.line(window, COLOURS['BLACK'], (0, i * GAP), (SCREEN_WIDTH, i * GAP))
-        for j in range(GRID_ROWS):
-            pygame.draw.line(window, COLOURS['BLACK'], (j * GAP, 0), (j * GAP, SCREEN_HEIGHT))
+    for j in range(GRID_ROWS):
+        pygame.draw.line(window, COLOURS['BLACK'], (j * GAP, 0), (j * GAP, SCREEN_HEIGHT - 200))  # adjust height here
+    for button in BUTTONS:  # make sure to draw buttons after grid lines
+        button.draw(window)
     pygame.display.update()
 
 def handle_mouse_click(grid, startCell, endCell):
     if pygame.mouse.get_pressed()[0]:
         y, x = pygame.mouse.get_pos()
-        row = y // GAP
-        col = x // GAP
-        cell = grid[row][col]
-        if not startCell and cell != endCell:
-            startCell = cell
-            startCell.makeStart()
-        elif not endCell and cell != startCell:
-            endCell = cell
-            endCell.makeEnd()
-        elif cell != endCell and cell != startCell:
-            cell.makeWall()
+        if y < SCREEN_HEIGHT - 200:  # restrict cell selection to grid area
+            row = y // GAP
+            col = x // GAP
+            cell = grid[row][col]
+            if not startCell and cell != endCell:
+                startCell = cell
+                startCell.makeStart()
+            elif not endCell and cell != startCell:
+                endCell = cell
+                endCell.makeEnd()
+            elif cell != endCell and cell != startCell:
+                cell.makeWall()
     return startCell, endCell
 
 def reset_grid(grid):
@@ -46,12 +91,3 @@ def reset_grid(grid):
         for cell in row:
             cell.reset()
     return startCell, endCell
-
-def pathfinding(window, grid, startCell, endCell, algorithm):
-    for row in grid:
-        for cell in row:
-            cell.updateNeighbours(grid)
-    if algorithm == 'astar':
-        pathfinder(lambda: draw(window, grid), grid, startCell, endCell, astar_priority)
-    elif algorithm == 'dijkstra':
-        pathfinder(lambda: draw(window, grid), grid, startCell, endCell, dijkstra_priority)
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from guihelpers import makeGrid, draw, handle_mouse_click, reset_grid, pathfinding
+from guihelpers import BUTTONS, pathfinding, make_grid, draw, reset_grid, handle_mouse_click
 
 if __name__ == "__main__":
+    pygame.init()
     window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Pathfinding Visualizer")
-    grid = makeGrid()
+    grid = make_grid()
     startCell = None
     endCell = None
     run = True
@@ -14,12 +15,17 @@ if __name__ == "__main__":
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a and startCell and endCell:
-                    pathfinding(window, grid, startCell, endCell, 'astar')
-                elif event.key == pygame.K_d and startCell and endCell:
-                    pathfinding(window, grid, startCell, endCell, 'dijkstra')
-                elif event.key == pygame.K_c:
-                    startCell, endCell = reset_grid(grid)
-        startCell, endCell = handle_mouse_click(grid, startCell, endCell)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                if pos[1] < SCREEN_HEIGHT - 200:  # restrict cell selection to grid area
+                    startCell, endCell = handle_mouse_click(grid, startCell, endCell)
+                else:
+                    for button in BUTTONS:
+                        if button.isOver(pos):
+                            if button.text == 'A*' and startCell and endCell:
+                                pathfinding(window, grid, startCell, endCell, 'astar')
+                            elif button.text == 'Dijkstra' and startCell and endCell:
+                                pathfinding(window, grid, startCell, endCell, 'dijkstra')
+                            elif button.text == 'Reset':
+                                startCell, endCell = reset_grid(grid)
     pygame.quit()
