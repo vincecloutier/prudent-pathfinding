41ef7c00ba3ed91c9032e467e98af5e02d7e5d43
vincecloutier <vincentcloutier@mail.com>
2023-07-08 13:39:23 -0400

    updated spacing

--- constants.py
+++ constants.py
@@ -3,10 +3,10 @@ GRID_WIDTH, GRID_HEIGHT = 50, 50
 GAP = SCREENWIDTH // GRID_WIDTH
 COLOURS = {
     'WHITE': (255, 255, 255),
-    'RED': (255, 0 , 0),
+    'RED': (255, 0, 0),
     'GREEN': (0, 255, 0),
-    'BLACK': (0 , 0, 0),
+    'BLACK': (0, 0, 0),
     'ORANGE': (255, 165, 0),
-    'PURPLE': (128, 0 , 128),
+    'PURPLE': (128, 0, 128),
     'TURQUOISE': (64, 224, 208)
 }
